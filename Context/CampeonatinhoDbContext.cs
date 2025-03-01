36a99710e3f4ca42fee35e793e7812facd65b526
Lucas Bardelli <lucas.bardelli@useargo.com>
2025-02-28 23:23:23 -0300

    Add project files.

---
+++ Context/CampeonatinhoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CampeonatinhoApp.Context
+{
+    public class CampeonatinhoDbContext : DbContext
+    {
+        public CampeonatinhoDbContext(DbContextOptions<CampeonatinhoDbContext> options) : base(options) { }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+using CampeonatinhoApp.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CampeonatinhoDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
